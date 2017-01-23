b89fc910be1f99ec4a9487e1cea07a16508b2a13
Igor Nikolaev <admin@Mac-mini-Igor-2.local>
2017-01-23 18:26:48 +0300

    new-project

---
+++ test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by Igor Nikolaev on 06.12.16.
+//  Copyright © 2016 Igor Nikolaev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
